f9848649a0df89e954ca91f7ba1dd54391a03965
Mark Wheelhouse <mjw03@doc.ic.ac.uk>
2018-09-25 13:22:44 +0100

    provided files for the 1920 Pintos lab exercise

---
+++ src/tests/devices/tests.h
@@ -0,0 +1,21 @@
+#ifndef TESTS_DEVICES_TESTS_H
+#define TESTS_DEVICES_TESTS_H
+
+void run_test (const char *);
+
+typedef void test_func (void);
+
+extern test_func test_alarm_single;
+extern test_func test_alarm_multiple;
+extern test_func test_alarm_simultaneous;
+extern test_func test_alarm_no_busy_wait;
+extern test_func test_alarm_one;
+extern test_func test_alarm_zero;
+extern test_func test_alarm_negative;
+
+void msg (const char *, ...);
+void fail (const char *, ...);
+void pass (void);
+
+#endif /* tests/devices/tests.h */
+
